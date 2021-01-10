1fe91d78b2c528da7d9d243aade8f7c8c158b9f6
Franci <algera.franci@gmail.com>
2021-01-10 18:40:35 +0100

    [feat]FA_UI/menu_#3
    pause menu

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -7,13 +7,31 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
 
+    public bool isPaused = false;
+    public GameObject pauseMenu;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P) && !isPaused)
+        {
+            Pause();
+        }
+        else if (Input.GetKeyDown(KeyCode.P) && isPaused)
+        {
+            UnPause();
+        }
+    }
     public void Pause()
     {
+        pauseMenu.SetActive(true);
         Time.timeScale = 0;
+        isPaused = true;
     }
     public void UnPause()
     {
         Time.timeScale = 1;
+        isPaused = false;
+        pauseMenu.SetActive(false);
     }
 
     public void LoadGameScene(string sceneToLoad)
