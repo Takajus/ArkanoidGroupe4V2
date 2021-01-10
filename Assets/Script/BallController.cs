a01d4cf42eaf122478d46e8095cbe49054c2b2d3
Franci <algera.franci@gmail.com>
2021-01-10 16:41:09 +0100

    [Feat]FA_Ball_#3
    Rebond de la balle contre les murs

--- Assets/Script/BallController.cs
+++ Assets/Script/BallController.cs
@@ -7,11 +7,15 @@ public class BallController : MonoBehaviour
     public float speedX;
     public float speedY;
     private bool ballLaunched = false;
+    private bool moveRight;
+    private bool moveUp;
+    private Rigidbody2D rb;
 
     void Start()
     {
         speedX = 0;
         speedY = 0;
+        rb = this.GetComponent<Rigidbody2D>();
     }
 
     void Update()
@@ -20,15 +24,59 @@ public class BallController : MonoBehaviour
         {
             BallStartThorw();
         }
-        this.transform.Translate(speedX, speedY, 0);
+
+        if(rb.velocity.x > 0)
+        {
+            moveRight = true;
+        }
+        else
+        {
+            moveRight = false;
+        }
+
+        if(rb.velocity.y > 0)
+        {
+            moveUp = true;
+        }
+        else
+        {
+            moveUp = false;
+        }
+
+        BallDirection();
     }
 
     public void BallStartThorw()
     {
-        float x = Random.Range(-1f, 1f);
+        float x = Random.Range(0f, .8f);
 
-        speedX = x * Time.deltaTime;
-        speedY = (1 - Mathf.Abs(x) )* Time.deltaTime;
+        speedX = x *10;
+        speedY = (1 - x) *10;
+        rb.velocity = new Vector2(speedX, speedY);
         ballLaunched = true;
+    }
+
+    private void BallDirection()
+    {
+        float x;
+        float y;
+        if (moveRight)
+        {
+            x = speedX;
+        }
+        else
+        {
+            x = -speedX;
+        }
+
+        if (moveUp)
+        {
+            y = speedY;
+        }
+        else
+        {
+            y = -speedY;
+        }
+        rb.velocity = new Vector2(x, y);
     }
 }
