47a86901aeaacd55db163ff744bb8f3013f9db71
Franci <algera.franci@gmail.com>
2021-01-10 18:25:49 +0100

    [Feat]FA_UI/Menu_#4
    Main menu avec buttons et main game scenes

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+    }
+    public void UnPause()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void LoadGameScene(string sceneToLoad)
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
