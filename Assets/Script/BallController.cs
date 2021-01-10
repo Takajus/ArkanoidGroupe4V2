7a9ccc2bd38b2c6be87631b77a1f76397724aac1
Franci <algera.franci@gmail.com>
2021-01-10 15:41:19 +0100

    [Feat]FA_Ball_#2
    Deplacement de la balle

---
+++ Assets/Script/BallController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public float speedX;
+    public float speedY;
+    private bool ballLaunched = false;
+
+    void Start()
+    {
+        speedX = 0;
+        speedY = 0;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !ballLaunched)
+        {
+            BallStartThorw();
+        }
+        this.transform.Translate(speedX, speedY, 0);
+    }
+
+    public void BallStartThorw()
+    {
+        float x = Random.Range(-1f, 1f);
+
+        speedX = x * Time.deltaTime;
+        speedY = (1 - Mathf.Abs(x) )* Time.deltaTime;
+        ballLaunched = true;
+    }
+}
