b65734799bdd1ca8f9aa0ac3487039095ac3193b
Franci <algera.franci@gmail.com>
2021-01-11 12:29:42 +0100

    [Feat]FA_UI/menu_#1
    health UI

--- Assets/Script/PlayerInfoManager.cs
+++ Assets/Script/PlayerInfoManager.cs
@@ -7,17 +7,46 @@ public class PlayerInfoManager : MonoBehaviour
 {
     public PlayerScoreController playerInfo;
     public TextMeshProUGUI scoreText;
+    [SerializeField] private GameObject[] health;
+    public GameObject playerhealthIcon;
 
     // Start is called before the first frame update
     void Start()
     {
         playerInfo.life = 3;
         playerInfo.score = 0;
+
     }
 
     // Update is called once per frame
     void Update()
     {
         scoreText.text = "Score : " + playerInfo.score;
+
+        HealthUpdate();
+    }
+
+    private void HealthUpdate()
+    {
+        for (int i = 0; i < playerInfo.life; i++)
+        {
+            health[i].SetActive(true);
+        }
+
+        if ( playerInfo.life == 2)
+        {
+            health[2].SetActive(false);
+        }else if ( playerInfo.life == 1)
+        {
+            health[2].SetActive(false);
+            health[1].SetActive(false);
+        }
+        else if ( playerInfo.life == 0)
+        {
+            health[2].SetActive(false);
+            health[1].SetActive(false);
+            health[0].SetActive(false);
+            //Game over <----------------------------------
+        }
     }
 }
